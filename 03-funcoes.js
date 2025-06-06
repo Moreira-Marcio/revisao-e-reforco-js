27ba7c340c885e52538632dfab7af68da5c5329c
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-06 09:20:44 -0300

    aplica função separador

--- 03-funcoes.js
+++ 03-funcoes.js
@@ -6,4 +6,25 @@ function exemplo1() {
     console.log("função nomeada");
 }
 
-exemplo1(); // chamada da função 
+exemplo1(); // chamada da função
+
+function separador() {
+    console.log("\n*************************************************\n");
+}
+
+
+separador(); // chamada da função separador
+
+//sintaxe tradicional, função anonima. Nessa sintaxe a ordem importa
+
+// aqui da erro por que nao pode ser posta antes da funçao ser declarada
+   // exemplo2 = (); 
+
+const exemplo2 = function() {
+    console.log("função anonima");
+};
+
+exemplo2(); // chamada (sempre apos declarar a função anonima) 
+
+separador();
+
