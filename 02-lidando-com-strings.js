c8539c322115ed02875421af788cbf07409ff0f1
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-06 08:50:08 -0300

    usa operador +=

--- 02-lidando-com-strings.js
+++ 02-lidando-com-strings.js
@@ -10,7 +10,13 @@ let estado = "SP";
 
 //concatenação tradicional usa o operador + meu nome é chapolin colorado, tenho 20 anos, moro em são paulo/sp
 
-let mensagem1 = "Meu nome é " + nome + " "+sobrenome + ", tenho" + " " + idade + " anos, moro em " + cidade + "/" + estado + ".";
+//let mensagem1 = "Meu nome é " + nome + " "+sobrenome + ", tenho" + " " + idade + " anos, moro em " + cidade + "/" + estado + ".";
  
+let mensagem1= "meu nome é "+ nome;
+mensagem1 += " " + sobrenome + ", tenho " + idade;
+mensagem1 += " anos, moro em " + cidade;
+mensagem1 += "/" + estado + ".";
+
+
 console.log(mensagem1);
 //template strings/literal 
