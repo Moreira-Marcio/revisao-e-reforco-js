96ffb8a537eaf031f88941bab8286e7772891f33
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-09 09:11:27 -0300

    cria modulo esm

---
+++ modulos-esm/exemplos.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/*modulos esm: ecmaScript modules (sistema atual, moderno) */
+
+function saudacao(nome) {
+    console.log ("ola"+ nome+"!");
+
+}
+
+const clientes = ["Maycon","Gabriel","Julia"];
+
+export {saudacao, clientes};
