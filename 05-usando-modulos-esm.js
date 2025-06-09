7e10e01b1b450a0ba7456bcbd658cf006d37f8dd
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-09 09:45:35 -0300

    acessa recursos modulo default

--- 05-usando-modulos-esm.js
+++ 05-usando-modulos-esm.js
@@ -12,3 +12,13 @@ saudacao("fulano de tal");
 console.log(clientes);
 
 separador();
+
+
+//acessando recursos do import alunos
+
+console.log(alunos[1]);
+console.log(alunos);
+
+for(const aluno of alunos){
+        console.log(alumos);
+}
