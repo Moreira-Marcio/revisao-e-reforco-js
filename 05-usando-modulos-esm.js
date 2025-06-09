ae6e8947110a19e8a9cf4cded352d15fba136461
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-09 09:22:18 -0300

    cria package.json

---
+++ 05-usando-modulos-esm.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//05 usando modulos esm
+
+
+//importação de multiplos recursos/modulos usando esm
+
+import { saudacao, clientes } from  "./modulos-esm/exemplos.js";
+
+saudacao("fulano de tal");
+
+console.log(clientes);
