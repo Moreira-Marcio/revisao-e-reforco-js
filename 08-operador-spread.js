f3c03c8a6683e41a29066ea215d32a1a37009b33
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-10 11:29:54 -0300

    spread para passar parametros

--- 08-operador-spread.js
+++ 08-operador-spread.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import { classificar } from "./modulos-esm/funcoes.js";
+import {separador} from "./modulos-esm/exemplos.js";
 /*sobre o operador spread
 usamos o spread para espalhar dados de arrays e objetos,
 o que ´pode ser util na chamada de funçoes e tambem na mesclagem  destas estruturas
@@ -15,4 +16,11 @@ const maisBandas = ["dream theater","savatage",...bandas];
 console.log(bandas);
 console.log(maisBandas);
 
+
+separador();
+
+// spread para espalhar parametros para uma função
+console.log(classificar(...maisBandas));
+
+separador();
 
