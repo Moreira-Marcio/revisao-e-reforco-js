a46ac3e41e630b9af1833c082041ac8e8589cae3
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-10 11:01:10 -0300

    cria operador rest.js

--- 07-operador-rest.js
+++ 07-operador-rest.js
@@ -1,2 +1,24 @@
 'use restrict'
 //07 operador rest
+
+
+/*Sobre o rest...
+usamos o rest para mesclar uma relação de parametros/argumentospara uma função*/
+
+function somar(...valores){
+ //   return valor1 + valor2;
+ //variavel acumuladora
+ let = total=0
+
+ //interação para somar e guardar o total
+ for (const valor of valore){
+    total += nalor
+ } 
+
+ return total;
+}
+
+let resultado1= somar (10,20);
+let resultado2 = somar(10,20,30);
+
+console.log (resultado);
