33947451519c42a29e616cf859e3ffd4330b1bed
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-10 11:49:49 -0300

    spread em objetos

--- 08-operador-spread.js
+++ 08-operador-spread.js
@@ -1,21 +1,20 @@
-'use strict';
+"use strict";
 
 import { classificar } from "./modulos-esm/funcoes.js";
-import {separador} from "./modulos-esm/exemplos.js";
+import { separador } from "./modulos-esm/exemplos.js";
 /*sobre o operador spread
 usamos o spread para espalhar dados de arrays e objetos,
 o que ´pode ser util na chamada de funçoes e tambem na mesclagem  destas estruturas
-*/ 
+*/
 
 //spread em arrays
 
-const bandas = ["yes" , "rush","pink floyd"];
+const bandas = ["yes", "rush", "pink floyd"];
 
-const maisBandas = ["dream theater","savatage",...bandas];
+const maisBandas = ["dream theater", "savatage", ...bandas];
 
 console.log(bandas);
 console.log(maisBandas);
-
 
 separador();
 
@@ -24,3 +23,20 @@ console.log(classificar(...maisBandas));
 
 separador();
 
+// spread em objetos
+
+const cliente = {
+  nome: "charlie chaplin",
+  cidade: "são paulo",
+  idade: 55,
+};
+
+const compra = {
+  ...cliente,
+  codigoPedido: "123xyz",
+  produto: ["bengala", "terno", "sapato"],
+  total: 2760.66,
+  nome: "paulo",
+};
+
+console.log(compra);
