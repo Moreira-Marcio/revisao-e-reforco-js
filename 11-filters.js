40a1a90fac672a9b150ef361aa7d9d67d16029e1
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-06-16 10:05:46 -0300

    cria filter e reduce

---
+++ 11-filters.js
@@ -0,0 +1,88 @@
+"use strict";
+
+import cursos from "./modulos-esm/cursos.js";
+import { separador } from "./modulos-esm/exemplos.js";
+
+/* filter (filtrar)
+Passa por elementos de um array e executa uma função callback visando
+retornar valores de acordo com uma ou mais condições, gerando um novo array. */
+
+// Exemplo 1: filtrar vendas que tenham batido uma meta
+const vendas = [1500, 3000, 500, 1000, 750, 5000, 100];
+const META = 1000;
+
+const vendasQueBateramMeta = vendas.filter((venda) => venda >= META);
+console.log(vendasQueBateramMeta);
+
+/* Se fosse fazer sem filter... */
+// const bateramMeta = [];
+// for (const venda of vendas) {
+//   if (venda >= META) {
+//     bateramMeta.push(venda);
+//   }
+// }
+// console.log(bateramMeta);
+
+separador();
+
+// Exemplo 2: filtrar serviços pendentes
+const servicos = [
+  { codigo: 1, tipo: "Limpeza", status: "Pendente" },
+  { codigo: 2, tipo: "Manutenção", status: "Pendente" },
+  { codigo: 3, tipo: "Reparo", status: "Pendente" },
+  { codigo: 4, tipo: "Consultoria", status: "Concluído" },
+  { codigo: 5, tipo: "Instalação", status: "Concluído" },
+];
+
+const servicosPendentes = servicos.filter(
+  ({ status }) => status === "Pendente"
+);
+
+console.log(servicosPendentes);
+
+separador();
+
+// Exemplo 3
+const cursosDesign = cursos.filter((curso) => {
+  return curso.categoria === "Design";
+});
+
+console.log(cursosDesign);
+
+separador();
+
+// Exemplo 4
+const cursosExcetoDesign = cursos.filter(
+  (curso) => curso.categoria !== "Design"
+);
+console.log(cursosExcetoDesign);
+
+separador();
+
+/* Exercício
+Gere um novo array com os cursos que atendam os seguintes critérios:
+- Categoria Front-End OU Mobile
+- Preços acima de 600 */
+const exercicioCursos = cursos.filter((curso) => {
+  return (
+    (curso.categoria === "Front-End" || curso.categoria === "Mobile") &&
+    curso.preco > 600
+  );
+});
+
+console.log("Quantidade de cursos do exercício: " + exercicioCursos.length);
+console.log(exercicioCursos);
+
+separador();
+
+/* 👿 Desafio ☠️ 
+Gerar um novo array com os cursos da categoria Back-End e COM O PREÇO atualizado
+com 10% de desconto. Para esta atividade você deverá combinar filter com map. */
+const desafio = cursos
+  .filter(({ categoria }) => categoria === "Back-End")
+  .map((curso) => ({
+    ...curso,
+    preco: curso.preco - curso.preco * 0.1,
+  }));
+
+console.log(desafio);
---
+++ 12-reduce.js
@@ -0,0 +1,42 @@
+"use strict";
+
+import cursos from "./modulos-esm/cursos.js";
+import { separador } from "./modulos-esm/exemplos.js";
+
+/* reduce (reduzir, redução, chegar a um resultado único)
+Passa por elementos de um array e reduz a um único valor, que pode ser
+uma soma, média, concatenação, etc.
+
+A função callback recebe dois parâmetros: o acumulador (valor que é acumulado até o momento), e o valor atual (elemento do array sendo processado).*/
+
+// Exemplo 1
+const valores = [10, 5, 50, 200, 1000];
+const total = valores.reduce((acumulador, valor) => acumulador + valor, 0);
+console.log(total);
+
+separador();
+
+//exercicio 1: realize a soma dos preços de todos os cursos
+//mostre log
+const valorCursos = cursos.reduce(
+  (acumulador, curso) => acumulador + curso.preco,
+  0
+);
+console.log(valorCursos);
+
+//exercicio 2: calcule a media dos preços de todos os cursos
+//mostre log
+
+//const media = valorCursos / cursos.length;
+const media = cursos.reduce((acumulador, { preco }) => {
+  return acumulador + preco / cursos.length;
+}, 0);
+console.log(media);
+
+//exercicio 3: clacule preço total dos cursosde front-end
+//mostre log
+const somaDosPrecosFront = cursos
+  .filter(({ categoria }) => categoria === "Front-end")
+  .reduce((acumulador, { preco }) => acummulador + preco, 0);
+
+console.log("soma dos precos dos cursos Front" + somaDosPrecosFront);
